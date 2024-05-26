9b2a5a63bed83efc3774825542f11943807f03e1
Nandakumar T <nandakumart07@gmail.com>
2024-05-26 19:22:47 +0530

    doct&userProfile,appointments

--- Controllers/doctorController.js
+++ Controllers/doctorController.js
@@ -1,3 +1,4 @@
+import Booking from '../models/BookingSchema.js'
 import Doctor from '../models/DoctorSchema.js'
 
 export const updateDoctor = async (req, res) => {
@@ -44,12 +45,31 @@ export const getAllDoctor = async (req, res) => {
                 ],
             }).select("-password");
         } else {
-            doctors = await Doctor.find({isApproved: 'approved'}).select("-password")
+            doctors = await Doctor.find({ isApproved: 'approved' }).select("-password")
         }
 
         res.status(200).json({ success: true, message: 'Doctors found', data: doctors })
     } catch (error) {
         res.status(500).json({ success: false, message: 'No doctor found' })
 
+    }
+}
+
+export const getDoctorProfile = async (req, res) => {
+    const doctorId = req.userId
+
+    try {
+        const doctor = await Doctor.findById(doctorId)
+
+        if (!doctor) {
+            return res.status(404).json({ success: false, message: 'Doctor not found' })
+        }
+
+        const { password, ...rest } = doctor._doc
+        const appointments=await Booking.find({doctor:doctorId})
+
+        res.status(200).json({ success: true, message: 'Profile info is getting', data: { ...rest,appointments } })
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Something went wrong, cannot get' })
     }
 }

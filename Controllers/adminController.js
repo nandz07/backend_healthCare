e78422d22b8edd151860cb99183ab3624062ed26
Nandakumar T <nandakumart07@gmail.com>
2024-08-11 22:21:54 +0530

    doctor approve

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import Admin from '../models/AdminSchema.js'
 import User from '../models/UserSchema.js'
+import Doctor from '../models/DoctorSchema.js'
 
 
 const generateToken = user => jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET_key, {
@@ -27,6 +28,45 @@ export const adminLogin = async (req, res) => {
     } catch (err) {
         console.log(err);
         return res.status(500).json({ status: false, message: "Failed to login" })
+    }
+}
+
+export const getAllDoctors = async (req, res) => {
+    try {
+        const { query } = req.query
+        let doctors;
+
+        if (query) {
+            doctors = await Doctor.find({
+                $or: [
+                    { name: { $regex: query, $options: "i" } },
+                    { specialization: { $regex: query, $options: "i" } }
+                ],
+            }).select("-password");
+        } else {
+            doctors = await Doctor.find({}).select("-password")
+        }
+
+        res.status(200).json({ success: true, message: 'Doctors found', data: doctors })
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'No doctor found' })
+
+    }
+}
+export const setDoctroAprroved = async (req, res) => {
+    const doctorId = req.params.id
+    try {
+        const doctor = await Doctor.findById(doctorId);
+        let updateDoctor
+        if (doctor.isApproved === 'approved') {
+            updateDoctor = await Doctor.findByIdAndUpdate(doctorId, { $set: { isApproved: 'pending' } }, { new: true })
+        } else {
+            updateDoctor = await Doctor.findByIdAndUpdate(doctorId, { $set: { isApproved: 'approved' } }, { new: true })
+        }
+        res.status(200).json({ success: true, message: 'Successfully updated', data: updateDoctor })
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'No doctor found' })
+
     }
 }
 
--- Routes/admin.js
+++ Routes/admin.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import {adminLogin} from '../Controllers/adminController.js'
+import {adminLogin, getAllDoctors, setDoctroAprroved} from '../Controllers/adminController.js'
 import { authenticate, restrict } from '../auth/verifyToken.js'
-import { getAllDoctor } from '../Controllers/doctorController.js'
 import { getAllUsers } from '../Controllers/userController.js'
 
 const router=express.Router()
 
 router.post('/adminLogin',adminLogin)
 router.get('/patients',authenticate,restrict(["admin"]),getAllUsers)
-router.get('/doctors',authenticate,restrict(["admin"]),getAllDoctor)
+router.get('/doctors',authenticate,restrict(["admin"]),getAllDoctors)
+router.patch('/doctor-approve/:id',authenticate,restrict(["admin"]),setDoctroAprroved)
 
 export default router
